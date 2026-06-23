d497115ffdb507c9fba9a75d1ac354e8f96a98d4
Monica Granbois <MonicaG@users.noreply.github.com>
2026-06-23 13:29:04 -0700

    simplified test-redirects.sh script

--- README.md
+++ README.md
@@ -17,8 +17,6 @@ The `test-redirects.py` script tests a subset of redirects and the error page.
 
 It tests against localhost.
 
-Test cases are defined in YAML test-cases.yaml file. 
-
 You can run it locally using Docker or Podman.
 
 ```bash
@@ -34,30 +32,14 @@ docker run --rm -p 2015:2015 -p 2016:2016 -p 2017:2017 -p 2018:2018 -p 2019:2019
 ```
 
 ```bash
-# Create python virtual environment and install requirements
-cd scripts
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+# Run all tests  
+./scripts/test-redirects.sh
 ```
 
-```bash
-# Run all tests against 
-python3 test-redirects.py 
-
-# Run tests for a specific service
-python3 test-redirects.py --service just-ask
-```
-
-Refer to the test-cases.yaml file to get the service names.
 
 ```bash
 # Or test individual redirects manually
 curl -I http://localhost:2015/sysdig-monitor-onboarding/
 ```
-
-### Adding a new test case
-
-Add new test cases to the test-cases.yaml file. No code changes should be required.
 
 
---
+++ scripts/test-redirects.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# Test script for redirect functionality
+
+set -euo pipefail
+
+DOCS_URL="http://localhost:2015/"
+SO_URL="http://localhost:2017/"
+RC_URL="http://localhost:2018/"
+JA_URL="http://localhost:2019/"
+
+test_url() {
+    local url_to_test="$1"
+    local expected_url="$2"
+    local response status location
+    
+    response=$(curl -s -w "%{http_code}\n%{redirect_url}\n" "$url_to_test")
+    status="$(printf '%s' "$response" | tail -n 2 | head -n 1)"
+    location="$(printf '%s' "$response" | tail -n 1)"
+
+    if [[ "$status" == "301" && "$location" == "$expected_url" ]]; then
+        echo "   ✅ OK - $location"
+    else
+        echo "   ❌ FAIL - $location (Status=$status)"
+        exit 1
+    fi
+}
+
+check_error_response() {
+    local url_to_test="$1"
+    response=$(curl -s -o /dev/null -w "%{http_code}\n" "${url_to_test}non-existent-path/")
+    status="$response"
+
+    if [[ "$status" == "404" ]]; then
+      echo "   ✅ 404 handling OK"
+    else
+      echo "   ❌ Expected 404 but got $status"
+      exit 1
+    fi
+}
+
+echo "Testing $DOCS_URL redirects"
+test_url "$DOCS_URL/" "https://developer.gov.bc.ca/docs/default/component/platform-developer-docs/"
+test_url "$DOCS_URL/sysdig-monitor-onboarding/" "https://developer.gov.bc.ca/docs/default/component/platform-developer-docs/docs/app-monitoring/sysdig-monitor-onboarding/"
+test_url "$DOCS_URL/rocketchat-etiquette/" "https://developer.gov.bc.ca/docs/default/component/bc-developer-guide/rocketchat/rocketchat-etiquette/"
+test_url "$DOCS_URL/platform-security-tools/" "https://developer.gov.bc.ca/docs/default/component/platform-developer-docs/docs/security-and-privacy-compliance/platform-security-tools/"
+
+echo "Testing $DOCS_URL error handling"
+check_error_response $DOCS_URL
+
+echo "Testing $SO_URL redirects"
+test_url "$SO_URL/" "https://github.com/bcgov/bcgov-community-discussions/discussions"
+test_url "$SO_URL/questions" "https://github.com/bcgov/bcgov-community-discussions/discussions"
+test_url "$SO_URL/questions/94/117" "https://github.com/bcgov/bcgov-community-discussions/discussions/16#discussioncomment-14942167"
+test_url "$SO_URL/q/100" "https://github.com/bcgov/bcgov-community-discussions/discussions/18"
+test_url "$SO_URL/a/121" "https://github.com/bcgov/bcgov-community-discussions/discussions/21#discussioncomment-14942197"
+
+echo "Testing $SO_URL error handling"
+check_error_response $SO_URL
+
+echo "Testing $RC_URL redirects"
+test_url "$RC_URL/" "https://bcgov.sharepoint.com/teams/developercommunity"
+test_url "$RC_URL/some/path?test=1" "https://bcgov.sharepoint.com/teams/developercommunity"
+
+echo "Testing $JA_URL redirects"
+test_url "$JA_URL/" "https://developer.gov.bc.ca/docs/default/component/bc-developer-guide/use-github-in-bcgov/bc-government-organizations-in-github/#single-sign-on"
+test_url "$JA_URL/some/other/path?foo=bar" "https://developer.gov.bc.ca/docs/default/component/bc-developer-guide/use-github-in-bcgov/bc-government-organizations-in-github/#single-sign-on"
